e7c0b194e65ccff79af06a7451b4a04d6b8f942b
Chris.Franklin <cfrank306@gmail.com>
2021-10-10 16:45:41 -0500

    added background and current-forcast box

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//global variables
+var location = "";
+var searchLocation = $("#search-value");
+var searchBtn = $("#search-btn");
+var clearBtn = $("#clear-btn");
+var currentLocation = $("#current-location");
+var currentCoords = $("#coord");
+var currentTemp = $("#temperature");
+var curretFeels = $("#feels-like");
+var currentMax = $("#temp-max");
+var currentMin = $("#temp-min");
+var currentPressure = $("#pressure");
+var currentHumidity = $("humidity");
+var currentWindSpeed = $("wind-speed");
+var currentWindDirection = $("wind-direction");
+var currentUvIndex = $("uv-index");
+var sCity = [];
